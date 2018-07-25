492a59f2a0d29acbb2c0cb1ae58e89f8d975566e
fymmit <blomqvist.timo@gmail.com>
2018-07-25 22:18:04 +0300

    Add Header, Footer and Main components.

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Main/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Main from "./components/Main.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Main,
+    Header,
+    Footer
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Prompt', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #a0a0a0;
 }
 </style>
---
+++ application/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+<div id="footer">
+    BOTTOM TEXT
+</div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+#footer {
+    margin: 60px;
+    font-size: xx-large;
+}
+</style>
