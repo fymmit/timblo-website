99198020ccc3b06ccf1ec832d01a615c0201fe19
fymmit <blomqvist.timo@gmail.com>
2018-07-25 22:52:19 +0300

    Use webpack boilerplate.

---
+++ site/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div id="header">
+    TOP TEXT
+</div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+#header {
+    margin: 60px;
+    font-size: xx-large;
+}
+</style>
